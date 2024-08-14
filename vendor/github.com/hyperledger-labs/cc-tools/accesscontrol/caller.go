ccda4aa479972d28c4caf96221917234f3c40c21
osamamagdy <osamamagdy174@gmail.com>
2024-08-14 20:36:38 +0300

    chore: remove vendor files

--- vendor/github.com/hyperledger-labs/cc-tools/accesscontrol/caller.go
+++
@@ -1,7 +0,0 @@
-package accesscontrol
-
-type Caller struct {
-	MSP        string            `json:"msp"`
-	OU         string            `json:"ou"`
-	Attributes map[string]string `json:"attributes"`
-}
--- vendor/github.com/hyperledger-labs/cc-tools/assets/getProp.go
+++
@@ -1,6 +0,0 @@
-package assets
-
-// GetProp returns the prop value. It returns nil if it doesn't exist.
-func (a Asset) GetProp(propTag string) interface{} {
-	return a[propTag]
-}
